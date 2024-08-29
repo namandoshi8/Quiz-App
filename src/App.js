6ccdfa72e4962edfb15f4a6349ee72fb160b04b5
namandoshi8 <workspace.naman@gmail.com>
2024-08-29 22:31:08 +0530

    React Quiz App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import "./App.css";
+// import DateCounter from "./Components/DateCounter";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Loader from "./Components/Loader";
+import Error from "./Components/Error";
+import Landing from "./Components/Landing";
+import Question from "./Components/Question";
 
-function App() {
+const initialState = {
+  questions: [],
+  // currentQuestion: 0,
+  // answers: [],
+  //staus : loading, error, ready, active, finished
+
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+
+    default:
+      throw new Error("Action not found");
+  }
+}
+
+export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numberOfQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <DateCounter /> */}
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <Landing numberOfQuestions={numberOfQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
+      </Main>
     </div>
   );
 }
-
-export default App;
